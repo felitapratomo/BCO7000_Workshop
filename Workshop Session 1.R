efc3a9a0ef65c1a8c8df90d46f7df4c98be1dc79
Felita Pratomo <felita.pratomo@live.vu.edu.au>
2022-02-22 21:41:02 +1100

    new file

---
+++ Workshop Session 1.R
@@ -0,0 +1,20 @@
+150/3
+a <- 10
+b <- "cool"
+c <- c(3,4,7,9)
+c
+
+summary(c)
+
+install.packages("tidyverse")
+library("tidyverse")
+
+rm(b)
+
+data(mtcars)
+
+head(mtcars)
+
+summary(mtcars)
+
+glimpse(mtcars)
